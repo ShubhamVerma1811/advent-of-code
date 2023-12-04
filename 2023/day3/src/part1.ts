15f70d2dd2071f61f6acbfb2871bddf9bfe402ed
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2023-12-04 07:47:52 +0530

    chore(ts): 2023/d3/p1

---
+++ 2023/day3/src/part1.ts
@@ -0,0 +1,47 @@
+import { loadInput } from '../../../helpers/file';
+
+function main() {
+  const file = loadInput(__dirname);
+
+  let sum = 0;
+
+  const lines = file.split('\n');
+  const reg = new RegExp(/\d+/g);
+  lines.map((line, lineNo) => {
+    const n = line.length;
+    const m = lines[0].length;
+
+    function isSymbol(i, j) {
+      if (!(0 <= i && i < n && 0 <= j && j < m)) {
+        return false;
+      }
+
+      // @ts-ignore
+      return lines?.[i]?.[j] !== '.' && isNaN(lines?.[i]?.[j]);
+    }
+
+    let match;
+    while ((match = reg.exec(line))) {
+      let i = match.index,
+        j = i + match[0].length - 1;
+
+      if (isSymbol(lineNo, i - 1) || isSymbol(lineNo, j + 1)) {
+        sum += parseInt(match[0]);
+        continue;
+      }
+
+      for (let k = i - 1; k <= j + 1; k++) {
+        if (isSymbol(lineNo - 1, k) || isSymbol(lineNo + 1, k)) {
+          sum += parseInt(match[0]);
+
+          continue;
+        }
+      }
+    }
+  });
+
+  return sum;
+}
+
+main();
+
---
+++ helpers/file.ts
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+
+function loadInput(dir) {
+  const filePath = path.join(dir, '/bin/input.txt');
+  return fs.readFileSync(filePath).toString();
+}
+
+export { loadInput };
