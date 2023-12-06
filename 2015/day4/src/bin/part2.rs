056380c7262f0b76217d8d17d76205f9f3528bb2
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2023-12-06 16:34:43 +0530

    chore(rust): 2015/d4

---
+++ 2015/day4/src/bin/part2.rs
@@ -0,0 +1,13 @@
+use md5::compute;
+
+fn main() {
+    let file = include_str!("input.txt");
+
+    let mut i = 0;
+
+    while !format!("{:x}", compute(format!("{}{}", file, i))).starts_with("000000") {
+        i += 1;
+    }
+
+    print!("{i}");
+}
